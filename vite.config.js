79543cdd922866c091b1f58466c20c7dc0d3f125
FunkyQai <qaiyumlee69@gmail.com>
2025-02-10 12:54:00 +0800

    Configure for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Valentines-Day-Card/", // Replace <repo-name> with your GitHub repo name
 })
